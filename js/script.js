3bc7fb8e192fe7e00d120e0cc98b76116c46e787
Simone Beaver <simone.beaver@verizon.com>
2019-06-11 15:24:53 -0500

    interval

--- js/script.js
+++ js/script.js
@@ -89,6 +89,6 @@ console.log(bgColor);
 }
 
 random_bg_color();
-
+setInterval(function(){ printQuote(); }, 25000);
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
